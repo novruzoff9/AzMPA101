d346eba469d4362b4eafa4c76858e486562241e7
User <yagmurnov2005@gmail.com>
2026-03-17 00:23:04 +0400

    Completed tasks 03.16

--- 03.16/03.16/Program.cs
+++ 03.16/03.16/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _03._16;
+
+SmartPhone smartPhone = new("Samsung", "A25", 256);
+SmartPhone smartPhone2 = new("Iphone", "A25", 256);
+SmartPhone smartPhone3 = new("Iphone", "A25", 128);
+smartPhone.InstallApp(15);
---
+++ 03.16/03.16/SmartPhone.cs
@@ -0,0 +1,63 @@
+﻿namespace _03._16;
+
+internal class SmartPhone
+{
+    public string Model;
+    public string Brand;
+    public int BatteryLevel;
+    public int StorageCapacity;
+    public int UsedStorage;
+    public bool IsLocked;
+
+    public SmartPhone (string brand, string model, int storageCapacity)
+    {
+        Brand = brand;
+        Model = model;
+        StorageCapacity = storageCapacity;
+        BatteryLevel = 0;
+        UsedStorage = 0;
+        IsLocked = true;
+    }
+    public void UnlockPhone ()
+    {
+        if (IsLocked)
+        {
+            IsLocked = false; 
+            Console.WriteLine("Telefon Achildi");
+        }
+        else
+            Console.WriteLine("Telefon artiq aciqdir");
+    }
+    public void InstallApp (int size)
+    {
+        if(UsedStorage + size <= StorageCapacity)
+        {
+            UsedStorage += size;
+            Console.WriteLine("Tetbiq yuklendi");
+        }
+        else
+            Console.WriteLine("Yaddashda bu tetbiq ucun bosh yer yoxdu");
+    }
+
+    public void ChargeBattery(int amount)
+    {
+        BatteryLevel += amount;
+
+        if (BatteryLevel > 100)
+        {
+            BatteryLevel = 100;
+        }
+        Console.WriteLine($"Batareya seviyyesi : {BatteryLevel}%");
+    }
+
+    public void PhoneInfo()
+    {
+        string lockStatus = IsLocked ? "Yes" : "No";
+
+        Console.WriteLine($"Brand: {Brand} ");
+        Console.WriteLine($"Model: {Model} ");
+        Console.WriteLine($"Battery Level : {BatteryLevel}%");
+        Console.WriteLine($"Storage : {StorageCapacity}GB Used: {UsedStorage}GB)");
+        Console.WriteLine($"Is Locked : {lockStatus}");
+    }
+}
